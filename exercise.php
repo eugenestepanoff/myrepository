c19b2e5a3a6476270f751ea8b0ea47e350a67d2a
eugenestepanoff <spaceisamyth13@gmail.com>
2022-03-18 22:19:23 +0600

    comment

--- exercise.php
+++ exercise.php
@@ -1,8 +1,8 @@
 <?php
-include 'functionsssssss.php';
-//напиши функцию, которая будет принимать строкой url (www.vk.com), а возвращать только домен (com)
-//функции которые понадобятся: explode, end
-$string = 'www.vk.com';
-$string2 = acc($string);
-var_dump($string2);
-
+//Написать функцию, которая принимает информацию о корзине (прайс, количество, скидка), которая возвращает информацию вида:
+//с в корзине x товаров на общую сумму y, Ваша скидка составляет z%
+$number = 30;
+$sum = 350;
+$disc = '59%';
+$format = 'You have %d products in your basket, costing %u dollars. Your discount is %s.';
+echo sprintf($format, $number, $sum, $disc);
